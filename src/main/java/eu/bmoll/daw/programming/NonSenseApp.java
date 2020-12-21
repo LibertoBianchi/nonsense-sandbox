7cdeb12e7dbec5e072e5fab878a813e1313322fa
LibertoBianchi <lbian@cifpfbmoll.eu>
2020-12-21 20:37:33 +0100

    feat: maven behavior

--- src/main/java/eu/bmoll/daw/programming/NonSenseApp.java
+++ src/main/java/eu/bmoll/daw/programming/NonSenseApp.java
@@ -1,14 +1,20 @@
 package eu.bmoll.daw.programming;
-
-import com.sun.tools.javac.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
 
 public class NonSenseApp {
     public static void main(String[] args) {
         boolean aux = StringUtils.equals("a","A");
         System.out.println("HOLA MUNDO"+aux);
+
         Scanner in = new Scanner (System.in);
-        String s = in.nextLine();
-        System.out.println("You entered string"+s);
+        boolean isExit= false;
+        while (!isExit) {
+            String s = in.nextLine();
+            System.out.println("You entered string" + s);
+            isExit= StringUtils.equals(s, EXIT_KEY);
+        }
         System.out.println("-END-");
     }
+
 }
